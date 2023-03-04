ded61f571a48427f007b0663c39638ddf8e398f9
Thanachok Rattanamora <schweppeweb@gmail.com>
2023-03-04 15:41:44 +0700

    Initial Commit

---
+++ FastChat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FastChat
+//
+//  Created by Schweppe on 4/3/2566 BE.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
