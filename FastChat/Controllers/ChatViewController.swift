081d053f63babd3d601b444cd5f26f51e8f10e04
Thanachok Rattanamora <schweppeweb@gmail.com>
2023-03-05 00:18:15 +0700

    Add ChatViewController

---
+++ FastChat/Controllers/ChatViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ChatViewController.swift
+//  FastChat
+//
+//  Created by Schweppe on 5/3/2566 BE.
+//
+
+import Foundation
+import UIKit
+
+class ChatViewController: UIViewController{
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    @IBOutlet weak var messageTextfield: UITextField!
+    
+    @IBAction func sendPressed(_ sender: UIButton) {
+        
+    }
+}
+
