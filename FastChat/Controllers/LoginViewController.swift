3daa06acf304e21883365aa17f717b5af94962b9
Thanachok Rattanamora <schweppeweb@gmail.com>
2023-03-05 00:13:05 +0700

    add views

---
+++ FastChat/Controllers/LoginViewController.swift
@@ -0,0 +1,18 @@
+//
+//  LoginViewController.swift
+//  FastChat
+//
+//  Created by Schweppe on 4/3/2566 BE.
+//
+
+import Foundation
+import UIKit
+class LoginViewController: UIViewController{
+    
+    @IBOutlet weak var emailTextfield: UITextField!
+    
+    @IBOutlet weak var passwordTextfield: UITextField!
+    
+    @IBAction func loginPressed(_ sender: UIButton) {
+    }
+}
---
+++ FastChat/Controllers/RegisterViewController.swift
@@ -0,0 +1,19 @@
+//
+//  RegisterViewController.swift
+//  FastChat
+//
+//  Created by Schweppe on 4/3/2566 BE.
+//
+
+import Foundation
+import UIKit
+class RegisterViewController: UIViewController{
+    
+    @IBOutlet weak var emailTextfield: UITextField!
+    
+    
+    @IBOutlet weak var passwordTextfield: UITextField!
+    
+    @IBAction func registerPressed(_ sender: UIButton) {
+    }
+}
